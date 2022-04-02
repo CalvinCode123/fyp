e6cb1fb158d9e4e6ec0677ba83bd2c76982eade1
unknown <c16487754@mydit.ie>
2022-04-02 22:15:30 +0100

    changing db

--- ClassTrail/settings.py
+++ ClassTrail/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'ClassTrail.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ClassTrailDB',
-        'USER' : 'postgres',
-        'PASSWORD' : 'LLqaz',
-        'HOST' : 'localhost',
-        'PORT' : '1234',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
