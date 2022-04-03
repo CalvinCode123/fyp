0a10432334eb69104509edd62ca48d9455f16d97
unknown <c16487754@mydit.ie>
2022-04-03 22:02:13 +0100

    change secret key to heroku vars

--- ClassTrail/settings.py
+++ ClassTrail/settings.py
@@ -136,9 +136,9 @@ django_heroku.settings(locals())
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 AWS_STORAGE_BUCKET_NAME = 'classtrailstorage'
 
