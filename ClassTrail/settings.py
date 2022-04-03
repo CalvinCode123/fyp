269f759bfc2dd71d34ecd7457758eddb8f5b1aab
unknown <c16487754@mydit.ie>
2022-04-03 21:58:49 +0100

    working app

--- ClassTrail/settings.py
+++ ClassTrail/settings.py
@@ -142,10 +142,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'classtrailstorage'
 
 S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-STATIC_URL = S3_URL
 
 AWS_QUERYSTRING_AUTH = False
