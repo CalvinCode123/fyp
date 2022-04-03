aec92a6215848a6698304bdba15881e06e3dd0c4
unknown <c16487754@mydit.ie>
2022-04-03 20:28:16 +0100

    added STATIC_URL config

--- ClassTrail/settings.py
+++ ClassTrail/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -142,4 +142,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
+S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = S3_URL
 AWS_QUERYSTRING_AUTH = False
